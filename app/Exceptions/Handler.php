6a32cfc69d84c75749053dfb07ff2c7f3aafe303
Jakabos <jumamiller@yahoo.com>
2023-09-09 11:00:56 +0300

    handle passport 401 errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,19 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponder;
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Laravel\Passport\Exceptions\AuthenticationException;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponder;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +34,20 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * @description render auth exception
+     * @param $request
+     * @param Exception|Throwable $e
+     * @return JsonResponse|RedirectResponse|Response|ResponseAlias
+     * @throws Throwable
+     */
+    public function render($request, Exception|Throwable $e): Response|JsonResponse|ResponseAlias|RedirectResponse
+    {
+        if ($e instanceof AuthenticationException) {
+            return $this->error($e->getMessage(),  ResponseAlias::HTTP_UNAUTHORIZED, $e->getTrace());
+        }
+        return parent::render($request, $e);
     }
 }
