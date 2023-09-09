5d625f4db041902142de3bf291c6a4697dd7ae08
Jakabos <jumamiller@yahoo.com>
2023-09-09 11:01:53 +0300

    add throw auth exception on the authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Laravel\Passport\Exceptions\AuthenticationException;
 
 class Authenticate extends Middleware
 {
@@ -12,6 +13,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        //if expected json response
+        if ($request->expectsJson()) {
+            return null;
+        } else {
+            //throw unauthenticated exception
+            throw new AuthenticationException();
+        }
     }
 }
