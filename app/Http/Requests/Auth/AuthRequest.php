ba939e7d711c5f256aeee920a9459ad59d8d6899
Jakabos <jumamiller@yahoo.com>
2023-09-09 10:54:05 +0300

    add auth request custom messages

--- app/Http/Requests/Auth/AuthRequest.php
+++ app/Http/Requests/Auth/AuthRequest.php
@@ -3,19 +3,18 @@
 namespace App\Http\Requests\Auth;
 
 use App\Http\Requests\Base\BaseRequest;
+use Illuminate\Contracts\Validation\ValidationRule;
 
 class AuthRequest extends BaseRequest
 {
     /**
      * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return self::getRules($this->route()->getActionMethod());
     }
-
     /**
      * @description get rules
      * @param string $method
@@ -37,5 +36,25 @@ class AuthRequest extends BaseRequest
             ],
         ];
         return $rules[$method];
+    }
+    /**
+     * @description get messages
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => __('validation.required', ['attribute' => 'email']),
+            'email.email' => __('validation.email', ['attribute' => 'email']),
+            'email.unique' => __('validation.unique', ['attribute' => 'email']),
+            'password.required' => __('validation.required', ['attribute' => 'password']),
+            'password.min' => __('validation.min.string', ['attribute' => 'password', 'min' => 6]),
+            'name.required' => __('validation.required', ['attribute' => 'name']),
+            'name.string' => __('validation.string', ['attribute' => 'name']),
+            'phone_number.required' => __('validation.required', ['attribute' => 'phone_number']),
+            'phone_number.string' => __('validation.string', ['attribute' => 'phone_number']),
+            'role.required' => __('validation.required', ['attribute' => 'role']),
+            'role.string' => __('validation.string', ['attribute' => 'role']),
+            'role.exists' => __('validation.exists', ['attribute' => 'role']),
+        ];
     }
 }
