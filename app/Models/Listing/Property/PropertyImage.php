76919ae11d2da26b3a1aa8625ee058fe635b4a21
Jakabos <jumamiller@yahoo.com>
2023-09-10 09:10:41 +0300

    image upload

--- app/Models/Listing/Property/PropertyImage.php
+++ app/Models/Listing/Property/PropertyImage.php
@@ -4,6 +4,7 @@ namespace App\Models\Listing\Property;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PropertyImage extends Model
@@ -18,4 +19,31 @@ class PropertyImage extends Model
         'updated_by',
         'deleted_by',
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
+
+    /**
+     * @param $query
+     * @param $filters
+     * @return mixed
+     */
+    public function scopeFilterBy($query,$filters)
+    {
+        return $query->where(function($query) use ($filters){
+            if(isset($filters['property_id'])){
+                $query->where('property_id',$filters['property_id']);
+            }
+            if(isset($filters['is_featured'])){
+                $query->where('is_featured',$filters['is_featured']);
+            }
+        })
+            ->orderBy($filters['sort_by']?? 'created_at',$filters['sort_order']?? 'desc')
+            ->paginate(isset($filters['per_page']) ? $filters['per_page'] : 10);
+    }
 }
--- database/migrations/2023_09_09_151011_create_property_inquiries_table.php
+++ database/migrations/2023_09_09_151011_create_property_inquiries_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->foreignId('updated_by')->nullable()->constrained('users');
             $table->foreignId('deleted_by')->nullable()->constrained('users');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
     /**
