5acc6d87d6c469cfae54df971096ac1037d0c7e2
Jakabos <jumamiller@yahoo.com>
2023-09-11 23:40:32 +0300

    database connections on readme

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/storage_link', function (){ Artisan::call('storage:link'); });
